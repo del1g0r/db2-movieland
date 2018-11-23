e260ba45ba2ef1b9cf764bf84a621220008a0639
Ihor <acc@1g0r.pp.ua>
2018-11-23 03:19:56 -0800

    [b-4] minor fix (unnecessary fx dependency)

--- src/main/java/com/study/movieland/entity/Movie.java
+++ src/main/java/com/study/movieland/entity/Movie.java
@@ -107,7 +107,7 @@ public class Movie {
                 Objects.equals(picturePath, movie.picturePath);
     }
 
-    public static class Builder implements javafx.util.Builder {
+    public static class Builder {
 
         private Movie movie;
 
@@ -155,7 +155,6 @@ public class Movie {
             return this;
         }
 
-        @Override
         public Movie build() {
             return movie;
         }

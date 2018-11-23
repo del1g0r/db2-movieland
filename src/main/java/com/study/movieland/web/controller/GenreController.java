0eb996904e2b838d64ba8f55138c1dcda46b9edc
Ihor <acc@1g0r.pp.ua>
2018-11-23 01:22:30 -0800

    [b-3] add produces APPLICATION_JSON_UTF8_VALUE

--- src/main/java/com/study/movieland/web/controller/GenreController.java
+++ src/main/java/com/study/movieland/web/controller/GenreController.java
@@ -3,6 +3,7 @@ package com.study.movieland.web.controller;
 import com.study.movieland.entity.Genre;
 import com.study.movieland.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class GenreController {
 
     private GenreService genreService;
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<Genre> getAll() {
         return genreService.getAll();
     }

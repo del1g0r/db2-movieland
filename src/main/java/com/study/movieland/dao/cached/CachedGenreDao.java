d98a97ef08d5814e3e9ee94d0959e48f5d8b7a23
Ihor <acc@1g0r.pp.ua>
2018-11-26 04:26:19 -0800

    [t-1] volatile + minor fixes

--- src/main/java/com/study/movieland/dao/cached/CachedGenreDao.java
+++ src/main/java/com/study/movieland/dao/cached/CachedGenreDao.java
@@ -4,8 +4,10 @@ import com.study.movieland.dao.GenreDao;
 import com.study.movieland.entity.Genre;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +15,13 @@ import java.util.List;
 @Primary
 public class CachedGenreDao implements GenreDao {
 
-    private GenreDao genreDao;
-    private List<Genre> genres;
+    private static final int DEFAULT_DELAY = 4 * 60 * 60 * 1000;
 
+    private GenreDao genreDao;
+    private volatile List<Genre> genres;
+
+    @PostConstruct
+    @Scheduled(fixedDelay = DEFAULT_DELAY, initialDelay = DEFAULT_DELAY)
     public void refresh() {
         this.genres = genreDao.getAll();
     }

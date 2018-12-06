d9e71a754750f97ee2c4c192bc7fa0a9af43ca18
Ihor <acc@1g0r.pp.ua>
2018-12-06 14:38:47 -0800

    [b-8] Add movie review

--- movieland-web/src/main/java/com/study/movieland/web/controller/ReviewController.java
+++ movieland-web/src/main/java/com/study/movieland/web/controller/ReviewController.java
@@ -1,11 +1,14 @@
 package com.study.movieland.web.controller;
 
 import com.study.movieland.dto.ReviewRequestDto;
+import com.study.movieland.entity.Role;
 import com.study.movieland.entity.Session;
 import com.study.movieland.service.ReviewService;
 import com.study.movieland.web.exception.NotAthorizedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.EnumSet;
 
 @RestController
 @RequestMapping(path = "review")
@@ -15,7 +18,7 @@ public class ReviewController {
 
     @PostMapping
     public void post(@RequestBody ReviewRequestDto reviewRequest, @RequestAttribute(required = false) Session session) {
-        if (session == null) {
+        if (session == null || !EnumSet.of(Role.USER, Role.ADMIN).contains(session.getUser().getRole())) {
             throw new NotAthorizedException();
         }
         reviewService.post(reviewRequest.getMovieId(), session.getUser().getId(), reviewRequest.getText());
